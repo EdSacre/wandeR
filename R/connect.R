c68a6cf012d291daeb68388cc497b7f0282d9c4d
EdSacre <eddiesacre@gmail.com>
2023-06-02 11:12:22 +0200

    general update

--- R/connect.R
+++ R/connect.R
@@ -6,7 +6,8 @@
 #'
 #' @param habitats A raster of species habitats or known locations.
 #' @param destinations A raster of the land or seascape. Values of 0 or NA are considered "barriers".
-#'     Values greater than 0 are considered valid location to travel through.
+#'     Values greater than 0 are considered valid locations to travel through.
+#' @param ncores The number of cores to use when running the model.
 #'
 #' @return A spatial raster of connectivity.
 #' @export
