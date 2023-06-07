db11d8936813048edb4db1426f7d669815d7d3d9
EdSacre <eddiesacre@gmail.com>
2023-06-07 11:59:44 +0200

    Update functions

--- R/connect.R
+++ R/connect.R
@@ -8,41 +8,56 @@
 #' @param destinations A raster of the land or seascape. Values of 0 or NA are considered "barriers".
 #'     Values greater than 0 are considered valid locations to travel through.
 #' @param maxdist The maximum dispersal distance of the species in map units.
+#' @param nthreads Specify the number of threads to use.
 #'
 #' @return A spatial raster of connectivity.
 #' @export
-#' @importFrom gdistance "costDistance"
-#' @importFrom gdistance "transition"
+#' @importFrom foreach "%dopar%"
+#' @importFrom utils "globalVariables"
 
-connect <- function(habitats, destinations, maxdist) {
+connect <- function(habitats, destinations, maxdist, nthreads = 1) {
+  # Enable parallel processing
+  clust <- parallel::makeCluster(nthreads, type = "PSOCK")
+  doParallel::registerDoParallel(cl = clust)
+  foreach::getDoParRegistered()
+
   # Checks
   habitats[habitats == 0] <- NA
+  destinations[destinations == 0] <- NA
 
+  # Define the dispersal kernel
   d <- maxdist * 0.3 # This defines how steep the dispersal kernel is - a value of 1 is almost linear
   a <- 1/d # this specifies that the dispersal kernel should taper towards 0 around the max dist
 
-  nhab <- length(habitats[habitats > 0])
-  ndest <- length(destinations[is.na(destinations) == FALSE])
-
+  # Create the transition layer from "gdistance" package and convert rasters to point
   trans <- gdistance::transition(destinations, transitionFunction = min, directions = 8) # Transition file
   trans <- gdistance::geoCorrection(trans, type = "c") # Geo-correct transition file
   hpoint <- raster::rasterToPoints(habitats, spatial = TRUE) # Set the origin points
   dpoint <- raster::rasterToPoints(destinations, spatial = TRUE) # Set the destination points
-  base <- c(1:ndest) * 0
 
-  for(i in 1:nhab){
-    cd <- gdistance::costDistance(trans, hpoint[i,], dpoint)
-    cd <- cd[1,]
-    cd[cd > maxdist] <- NA
-    cd[is.na(cd) == FALSE] <- exp(-a*(cd[is.na(cd) == FALSE]))
-    cd[is.na(cd) == TRUE] <- 0
-    #cd <- cd * speco[i,]$habvalue # Add this line to give higher connectivity values to higher habitat quality/coverage
-    base <- base+cd
+  nhab <- length(hpoint)
+  ndest <- length(dpoint)
+
+  blocks <- seq(from = 0, to = nhab, length.out = nthreads+1)
+  blocks <- round(blocks)
+
+  pfcd <- foreach::foreach(p=1:nthreads, .combine = '+', .packages="foreach") %dopar% {
+    k <- (blocks[p]+1):blocks[p+1]
+    connect.helper(nhab = length(k),
+                    ndest = ndest,
+                    hpoint = hpoint[k,],
+                    dpoint = dpoint,
+                    trans = trans,
+                    maxdist = maxdist,
+                    a = a)
   }
 
-  out <- destinations * 0
-  out[is.na(out) == F] <- base
-  return(out)
+  parallel::stopCluster(cl = clust) # Stop cluster
+
+  # Send outputs to raster
+  ras <- destinations
+  ras[is.na(ras) == F] <- pfcd
+  return(ras)
 }
 
-
+if(getRversion() >= "2.15.1")  utils::globalVariables("p")
